765926221db5284b5d9100d372397aed30cbbaf1
hoagduc27 <85150430+hoagduc27@users.noreply.github.com>
2022-11-25 14:29:28 +0700

    crud_khuyen_mai

---
+++ src/Service/Interface/IKhuyenMaiService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Service.Interface;
+
+import DomainModel.KhuyenMai;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author TRUNG DUC
+ */
+public interface IKhuyenMaiService {
+    ArrayList<KhuyenMai> getAll();
+    
+    Boolean them(KhuyenMai km);
+    
+    Boolean sua(KhuyenMai km,String ma);
+    
+    Boolean xoa(String ma);
+}
